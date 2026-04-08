a0a9a1ea562f1d79f3383bd4ed9f31e0e43efc3d
Pratham <pratham@Prathams-MacBook-Air-3.local>
2026-04-08 22:25:42 +0530

    pipeline

---
+++ app.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import os
+from datetime import datetime
+
+os.makedirs("output", exist_ok=True)
+
+img = np.zeros((400,600,3), dtype=np.uint8)
+
+cv2.putText(
+    img,
+    "Jenkins Docker Build Success",
+    (30,200),
+    cv2.FONT_HERSHEY_SIMPLEX,
+    0.8,
+    (0,255,0),
+    2
+)
+
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+cv2.putText(
+    img,
+    timestamp,
+    (30,250),
+    cv2.FONT_HERSHEY_SIMPLEX,
+    0.6,
+    (255,255,255),
+    1
+)
+
+cv2.imwrite("output/detections.jpg", img)
+
+print("Output saved to output/detections.jpg")
